a55c89ab2415a689d2a83f5176e43db8802ec7b8
mateidiana <mateidiana10@gmail.com>
2024-05-03 10:32:41 +0300

    ADDED start data to repo

--- Repository-Kunden/KundenRepository.cpp
+++ Repository-Kunden/KundenRepository.cpp
@@ -4,6 +4,10 @@
 
 #include "KundenRepository.h"
 
+KundenRepository::KundenRepository() {
+    startData();
+}
+
 void KundenRepository::erstelleKunde(const Kunde& kunde, bool istMitarbeiter) {
     if (!istMitarbeiter) {
         throw invalid_argument("Nur Mitarbeiter dürfen Kunden erstellen.");
@@ -54,4 +58,19 @@ void KundenRepository::anonymisiereKunde(const Kunde& kunde, bool istMitarbeiter
         }
     }
     throw invalid_argument("Kunde nicht gefunden");
+}
+
+vector<Kunde> KundenRepository::getAll() { return kunden_; }
+
+void KundenRepository::startData() {
+    kunden_.push_back(Kunde("1","Popescu","Bob","bob1@yahoo.com","1234567892","Plopilor 1","", false));
+    kunden_.push_back(Kunde("2","Georgescu","Zob","zob@yahoo.com","2341567345","Moldoveanu 15","", false));
+    kunden_.push_back(Kunde("3","Florescu","Dob","dob25@yahoo.com","5678345123","Ariesului 112","", false));
+    kunden_.push_back(Kunde("4","Grigorescu","Cob","ciob15@yahoo.com","4567123234","Bucium 20","", false));
+    kunden_.push_back(Kunde("5","Vladescu","Ana","anavld@yahoo.com","2345123897","Cisnadiei 2","", false));
+    kunden_.push_back(Kunde("6","Andrei","Maria","andreimaria5@yahoo.com","5678456098","Baciului 4","", false));
+    kunden_.push_back(Kunde("7","George","Mihai","mihaig@yahoo.com","4567345234","Negoi 12","", false));
+    kunden_.push_back(Kunde("8","Mihaelescu","Laura","lauram@yahoo.com","5678456345","Florilor 15","", false));
+    kunden_.push_back(Kunde("9","Neamtu","Andreea","neamtuandreea@yahoo.com","4567234123","Farului 1","", false));
+    kunden_.push_back(Kunde("10","Fader","Bianca","biancafad@yahoo.com","6789467125","Gradinii 45","", false));
 }
--- Repository-Kunden/KundenRepository.h
+++ Repository-Kunden/KundenRepository.h
@@ -14,12 +14,15 @@ class KundenRepository {
 
 private:
     vector<Kunde> kunden_;
+    void startData();
 
 public:
+    KundenRepository();
     void erstelleKunde(const Kunde& kunde, bool istMitarbeiter);
     void aendereKunde(const Kunde& kunde, bool istMitarbeiter);
     void loscheKunde(const Kunde& kunde, bool istMitarbeiter);
     void anonymisiereKunde(const Kunde& kunde, bool istMitarbeiter);
+    vector<Kunde> getAll();
 };
 
 
