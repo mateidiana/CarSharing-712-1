5fb02f00a0158cc4a14305cff71500207c59bf45
tudorriva <tudorivanceastefan@gmail.com>
2024-05-14 16:46:40 +0300

    added create and modify Car

--- Repository-Car/CarRepository.h
+++ Repository-Car/CarRepository.h
@@ -5,4 +5,20 @@
 #ifndef CARSHARING_712_1_CARREPOSITORY_H
 #define CARSHARING_712_1_CARREPOSITORY_H
 
+#include <vector>
+#include "../Domain-Car/Car.h"
+
+using namespace std;
+
+class CarManager {
+private:
+    vector<Car> cars;
+
+public:
+    void createCar(const Car& car);
+    void modifyCar(const string& licensePlate, const Car& newCarData);
+    void deleteCar(const string& licensePlate);
+    void deactivateCar(const string& licensePlate);
+};
+
 #endif //CARSHARING_712_1_CARREPOSITORY_H
