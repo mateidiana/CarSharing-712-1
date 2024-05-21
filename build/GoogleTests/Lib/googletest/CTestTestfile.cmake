5a7a55072beca59552716a2224dd0ad2a119e75d
tudorriva <tudorivanceastefan@gmail.com>
2024-05-21 18:52:28 +0300

    Add GoogleTests, Repository-Car, and Domain-Car directories to CMakeLists.txt

---
+++ build/GoogleTests/Lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/tudor/Documents/carsharing app/CarSharing-712-1/GoogleTests/Lib/googletest
+# Build directory: C:/Users/tudor/Documents/carsharing app/CarSharing-712-1/build/GoogleTests/Lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
