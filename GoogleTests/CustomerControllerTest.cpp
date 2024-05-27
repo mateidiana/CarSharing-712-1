38301a87b3e2cd717489614e8038ead88896c302
Maier Georgiana <149155244+giamaier03777@users.noreply.github.com>
2024-05-27 20:44:46 +0300

    Validation- and gdpr-Tests

--- GoogleTests/CustomerControllerTest.cpp
+++ GoogleTests/CustomerControllerTest.cpp
@@ -1,59 +1,89 @@
-#include "../Repository-Car/CarRepository.h"
+#include "../Repository-Customers/CustomerRepository.h"
+#include "../Domain-User/User.h"
+#include "../Controller-Customers/CustomerController.h"
 #include "gtest/gtest.h"
+
 
 class CustomerControllerTest : public ::testing::Test {
 protected:
-    CarRepository repository;
-    Car car1 = Car("plate1", "Model1", "Brand1", 2000, 10000, 100.0, "Gasoline", "Manual", "Red", "No remarks");
-    Car car2 = Car("plate2", "Model2", "Brand2", 2001, 20000, 200.0, "Diesel", "Automatic", "Blue", "No remarks");
+    CustomerRepository customerRepo;    //stocare clienti
+    CustomerController controller;
+
+    Customer customer1 = Customer("1", "John", "Mike", "john.mike@example.com", "+1234567890", "Address1", "Remarks1", false, "password");
+    Customer customer2 = Customer("2", "Jane", "Smith", "jane.smith@example.com", "+0987654321", "Address2", "Remarks2", false, "password");
+
+    CustomerControllerTest() : controller(CustomerRepository()) {}
 
     void SetUp() override {
-        car1.setOrdered(false);
-        car2.setOrdered(false);
-        repository.addCar(car1);
-        repository.addCar(car2);
+        customerRepo.createCustomer(customer1, false);
+        customerRepo.createCustomer(customer2, false);
+        controller = CustomerController(std::move(customerRepo));   //mutare repo in controller
     }
 };
 
-TEST_F(CustomerControllerTest, AddCarIncreasesRepositorySize) {
-    size_t initialSize = repository.returnList().size();
-    Car car3 = Car("plate3", "Model3", "Brand3", 2002, 30000, 300.0, "Electric", "Automatic", "Green", "No remarks");
-    repository.addCar(car3);
-    ASSERT_EQ(repository.returnList().size(), initialSize + 1);
+TEST_F(CustomerControllerTest, IsEmailTakenReturnsTrueForExistingEmail) {
+    ASSERT_TRUE(controller.isEmailTaken("john.mike@example.com"));  //luat
 }
 
-TEST_F(CustomerControllerTest, UpdateCarChangesCarData) {
-    Car newCarData = Car("plate1", "UpdatedModel", "UpdatedBrand", 2005, 15000, 150.0, "Hybrid", "Manual", "Yellow", "Updated remarks");
-    repository.updateCar("plate1", newCarData);
-    auto cars = repository.returnList();
-    auto it = std::find_if(cars.begin(), cars.end(), [](const Car& car) {
-        return car.getLicensePlate() == "plate1";
-    });
-    ASSERT_NE(it, cars.end());
-    ASSERT_EQ(it->getModel(), "UpdatedModel");
-    ASSERT_EQ(it->getBrand(), "UpdatedBrand");
+TEST_F(CustomerControllerTest, IsEmailTakenReturnsFalseForNonExistingEmail) {
+    ASSERT_FALSE(controller.isEmailTaken("new.email@example.com")); //neluat
 }
 
-TEST_F(CustomerControllerTest, RemoveCarDecreasesRepositorySize) {
-    size_t initialSize = repository.returnList().size();
-    ASSERT_NO_THROW(repository.removeCar("plate1"));
-    ASSERT_EQ(repository.returnList().size(), initialSize - 1);
+TEST_F(CustomerControllerTest, IsPhoneNumberValidReturnsFalseForInvalidNumber) {
+    ASSERT_FALSE(controller.isPhoneNumberValid("123-456-7890"));    //invalid
+    ASSERT_FALSE(controller.isPhoneNumberValid("phone1234"));     //invalid
+    ASSERT_FALSE(controller.isPhoneNumberValid("+123 456 78A9"));     //invalid
 }
 
-TEST_F(CustomerControllerTest, RemoveCarThrowsExceptionWhenCarNotFound) {
-    ASSERT_THROW(repository.removeCar("nonexistent_plate"), std::runtime_error);
+TEST_F(CustomerControllerTest, IsPhoneNumberValidReturnsTrueForValidNumber) {   //valide toate
+    ASSERT_TRUE(controller.isPhoneNumberValid("+1234567890"));
+    ASSERT_TRUE(controller.isPhoneNumberValid("1234567890"));
+    ASSERT_TRUE(controller.isPhoneNumberValid("+123 456 7890"));
 }
 
-TEST_F(CustomerControllerTest, DeactivateCarChangesCarStatus) {
-    ASSERT_NO_THROW(repository.deactivateCar("plate2"));
-    auto cars = repository.returnList();
-    auto it = std::find_if(cars.begin(), cars.end(), [](const Car& car) {
-        return car.getLicensePlate() == "plate2";
-    });
-    ASSERT_NE(it, cars.end());
-    ASSERT_FALSE(it->isActive());
+TEST_F(CustomerControllerTest, IsEmailFormaValidReturnsFalseForInvalidEmail) {  //invalide
+    ASSERT_FALSE(controller.isEmailFormaValid("plainaddress"));
+    ASSERT_FALSE(controller.isEmailFormaValid("@missingusername.com"));
+    ASSERT_FALSE(controller.isEmailFormaValid("username@.com"));
 }
 
-TEST_F(CustomerControllerTest, DeactivateCarThrowsExceptionWhenCarNotFound) {
-    ASSERT_THROW(repository.deactivateCar("nonexistent_plate"), std::runtime_error);
+TEST_F(CustomerControllerTest, IsEmailFormaValidReturnsTrueForValidEmail) { //valide
+    ASSERT_TRUE(controller.isEmailFormaValid("email@example.com"));
+    ASSERT_TRUE(controller.isEmailFormaValid("firstname.lastname@example.com"));
+    ASSERT_TRUE(controller.isEmailFormaValid("email@subdomain.example.com"));
+}
+//gdprDeleted = false, toate campurile completate
+TEST_F(CustomerControllerTest, AddCustomerThrowsNoErrorWhenGdprFalseAndAllFieldsFilled) {
+    ASSERT_NO_THROW(controller.addCustomer("3", "Alice", "Wonder", "alice.wonder@example.com", "password",
+                                           "+1122334455", "Address3", "Remarks3", false, false));
+}
+
+//gdprDeleted = false, campuri obligatorii necompletate
+TEST_F(CustomerControllerTest, AddCustomerThrowsErrorWhenGdprFalseAndFieldsMissing) {
+    ASSERT_THROW(controller.addCustomer("", "Alice", "Wonder", "alice.wonder@example.com", "password",
+                                        "+1122334455", "Address3", "Remarks3", false, false), std::runtime_error);
+    ASSERT_THROW(controller.addCustomer("3", "", "Wonder", "alice.wonder@example.com", "password",
+                                        "+1122334455", "Address3", "Remarks3", false, false), std::runtime_error);
+    ASSERT_THROW(controller.addCustomer("3", "Alice", "", "alice.wonder@example.com", "password",
+                                        "+1122334455", "Address3", "Remarks3", false, false), std::runtime_error);
+    ASSERT_THROW(controller.addCustomer("3", "Alice", "Wonder", "", "password",
+                                        "+1122334455", "Address3", "Remarks3", false, false), std::runtime_error);
+    ASSERT_THROW(controller.addCustomer("3", "Alice", "Wonder", "alice.wonder@example.com", "",
+                                        "+1122334455", "Address3", "Remarks3", false, false), std::runtime_error);
+    ASSERT_THROW(controller.addCustomer("3", "Alice", "Wonder", "alice.wonder@example.com", "password",
+                                        "", "Address3", "Remarks3", false, false), std::runtime_error);
+}
+
+//gdprDeleted = true, name + lastName completate
+TEST_F(CustomerControllerTest, AddCustomerThrowsNoErrorWhenGdprTrueAndNameFieldsFilled) {
+    ASSERT_NO_THROW(controller.addCustomer("", "Alice", "Wonder", "", "",
+                                           "", "", "Remarks3", true, false));
+}
+
+//gdprDeleted = true, name + lastName necompletate
+TEST_F(CustomerControllerTest, AddCustomerThrowsErrorWhenGdprTrueAndNameFieldsMissing) {
+    ASSERT_THROW(controller.addCustomer("", "", "Wonder", "", "",
+                                        "", "", "Remarks3", true, false), std::runtime_error);
+    ASSERT_THROW(controller.addCustomer("", "Alice", "", "", "",
+                                        "", "", "Remarks3", true, false), std::runtime_error);
 }
