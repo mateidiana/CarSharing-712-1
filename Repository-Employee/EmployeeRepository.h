46ae6f68c65566f7f00f65a101344f3f6536aa74
ana kogalniceanu <anastef10@yahoo.com>
2024-05-28 00:00:09 +0300

    solved conflicts

--- Repository-Employee/EmployeeRepository.h
+++ Repository-Employee/EmployeeRepository.h
@@ -4,47 +4,41 @@
 
 #ifndef CARSHARING_712_1_EMPLOYEEREPOSITORY_H
 #define CARSHARING_712_1_EMPLOYEEREPOSITORY_H
+
 #include <vector>
 #include <stdexcept>
 #include "../Domain-Employee/Employee.h"
-class Employee;
 #include "../Domain-Admin/Admin.h"
-class Admin;
 
-class EmployeeRepository{
+class EmployeeRepository {
 
 private:
-
-    vector<Employee> employees_;
-    vector<Admin> admins_;
+    std::vector<Employee> employees_;
+    std::vector<Admin> admins_;
 
 public:
+    EmployeeRepository(const std::vector<Employee> &employees_, const std::vector<Admin> &admins_) : employees_(employees_), admins_(admins_) {};
 
-    EmployeeRepository(const vector<Employee> &employees_, const vector<Admin> &admins_) : employees_(employees_), admins_(admins_) {};
     void viewSalary(Employee &employee);
     void adjustSalary(Employee &employee, double newSalary);
     void assignAdminRights(Employee &employee);
-    void resetPassword(Employee &employee, string newPassword);
+    void resetPassword(Employee &employee, std::string newPassword);
     void removeAdminRights(Admin &admin);
 
     void addAdmin(Admin &admin);
-
     void addEmployee(Employee &employee);
+    void removeEmployee(const std::string &email);
+    void removeAdmin(const std::string &email);
 
-    void removeEmployee(const string &email);
+    std::vector<Employee> searchEmployeesByBirthDateRange(const std::string &startDate, const std::string &endDate) const;
+    std::vector<Employee> searchEmployeesByNameAndLastName(const std::string &name, const std::string &lastName) const;
+    std::vector<Employee> getAllEmployees() const;
 
-    void removeAdmin(const string &email);
+    Employee* searchEmployeeByEmail(const std::string &email);
+    Employee* searchEmployeeByAbbreviation(const std::string &abbreviation);
 
-    vector<Employee> searchEmployeesByBirthDateRange(const string &startDate, const string &endDate) const;
-
-    vector<Employee> searchEmployeesByNameAndLastName(const string &name, const string &lastName) const;
-
-    vector<Employee> getAllEmployees() const;
-
-    Employee *searchEmployeeByEmail(const string &email);
-
-    Employee *searchEmployeeByAbbreviation(const string &abbreviation);
+    void setupUnitTests();
+    void startData();
 };
-
 
 #endif //CARSHARING_712_1_EMPLOYEEREPOSITORY_H
