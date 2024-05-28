3fee61f36601601f209abc4e4033f80cb62189bd
LuchianovAntoniu-Aureliu <150062773+LuchianovAntoniu-Aureliu@users.noreply.github.com>
2024-05-28 18:39:27 +0300

    Task 2, Echipa 1, M3.1 - M3.4, M4.1

---
+++ controller.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "domain.cpp"
+using namespace std;
+
+class EmployeeController {
+private:
+    std::vector<Employee> employees;
+public:
+    void addEmployee(const Employee &employee) {
+        auto it = std::find_if(employees.begin(), employees.end(), [&](const Employee &e) {
+            return e.get_firstName() == employee.get_firstName() && e.get_lastName() == employee.get_lastName();
+        });
+        if (it != employees.end()) {
+            employees.push_back(employee);
+        }
+    }
+
+    void deleteEmployee(const std::string &firstName, const std::string &lastName) {
+        auto it = std::remove_if(employees.begin(), employees.end(), [&](const Employee &e) {
+            return e.get_firstName() == firstName && e.get_lastName() == lastName;
+        });
+        employees.erase(it, employees.end());
+    }
+
+    void sortByFirstName() {
+        std::sort(employees.begin(), employees.end(), [](const Employee& e1, const Employee& e2) {
+            return e1.get_firstName() < e2.get_firstName();
+        });
+    }
+
+    void sortByLastName() {
+        std::sort(employees.begin(), employees.end(), [](const Employee& e1, const Employee& e2) {
+            return e1.get_lastName() < e2.get_lastName();
+        });
+    }
+
+    std::vector<Employee> get_employees() const {
+        return employees;
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "controller.cpp"
+using namespace std;
+
+void read(ifstream& inputFile, string& firstName, string& lastName, string& emailAddress, string& occupation, string& car, string& model) {
+    getline(inputFile, firstName, ' ');
+    getline(inputFile, lastName, ' ');
+    getline(inputFile, emailAddress, ' ');
+    getline(inputFile, occupation, ' ');
+    getline(inputFile, car, ' ');
+    getline(inputFile, model, ' ');
+}
+
+int main() {
+    EmployeeController controller;
+    int nr;
+    string firstName, lastName, emailAddress, occupation, car, model;
+    ifstream inputFile("file.txt");
+    if(!inputFile.is_open()) {
+        cerr << "Failed to open input file." << endl;
+        return 1;
+    }
+    inputFile >> nr;
+    inputFile.ignore();
+    for(int i = 0; i < nr; ++i) {
+        read(inputFile, firstName, lastName, emailAddress, occupation, car, model);
+        Employee newEmployee(firstName, lastName, emailAddress, occupation, car, model);
+        controller.addEmployee(newEmployee);
+        inputFile.ignore();
+        cout << firstName << " " << lastName << " " << emailAddress << " " << occupation << " " << car << " " << model;
+    }
+    inputFile.close();
+    return 0;
+}
