a4159f3056fc0e61fe00d03dcb97f6476fe97710
LuchianovAntoniu-Aureliu <150062773+LuchianovAntoniu-Aureliu@users.noreply.github.com>
2024-05-28 18:09:12 +0300

    Task 2, Echipa 1, M3.1 - M3.4, M4.1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
-#include "Controller/Controller.h"
+#include <fstream>
+#include <string>
+#include "controller.cpp"
+using namespace std;
+
+void read(ifstream& inputFile, string& firstName, string& lastName, string& EmailAddress, string& occupation, string& car, string& model) {
+    getline(inputFile, firstName, ' ');
+    getline(inputFile, lastName, ' ');
+    getline(inputFile, EmailAddress, ' ');
+    getline(inputFile, occupation, ' ');
+    getline(inputFile, car, ' ');
+    getline(inputFile, model, ' ');
+}
+
 int main() {
-    std::cout << "salut !" << std::endl;
-    Controller c;
-    c.idk();
-    std::cout<<"Test";
+    EmployeeController controller;
+    int nr;
+    string firstName, lastName, EmailAddress, occupation, car, model;
+    ifstream inputFile("file.txt");
+    if(!inputFile.is_open()) {
+        cerr << "Failed to open input file." << endl;
+        return 1;
+    }
+    inputFile >> nr;
+    inputFile.ignore();
+    for(int i = 0; i < nr; ++i) {
+        read(inputFile, firstName, lastName, EmailAddress, occupation, car, model);
+        Employee newEmployee(firstName, lastName, EmailAddress, occupation, car, model);
+        controller.addEmployee(newEmployee);
+        inputFile.ignore();
+        cout << firstName << " " << lastName << " " << EmailAddress << " " << occupation << " " << car << " " << model;
+    }
+    inputFile.close();
     return 0;
 }
