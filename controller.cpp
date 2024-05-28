d80ca3b04f1a8d9c95923b0a40344d2f8842c888
LuchianovAntoniu-Aureliu <150062773+LuchianovAntoniu-Aureliu@users.noreply.github.com>
2024-05-28 19:34:59 +0300

    Update controller.cpp

--- controller.cpp
+++ controller.cpp
@@ -9,35 +9,35 @@ class EmployeeController {
 private:
     std::vector<Employee> employees;
 public:
-    void addEmployee(const Employee &employee) {
-        auto it = std::find_if(employees.begin(), employees.end(), [&](const Employee &e) {
-            return e.get_firstName() == employee.get_firstName() && e.get_lastName() == employee.get_lastName();
+    void addEmployee(const Employee& employee) {
+        auto it = std::find_if(employees.begin(), employees.end(), [&](const Employee& e) {
+            return e.getName() == employee.getName() && e.getLastName() == employee.getLastName();
         });
-        if (it != employees.end()) {
+        if(it != employees.end()) {
             employees.push_back(employee);
         }
     }
 
     void deleteEmployee(const std::string &firstName, const std::string &lastName) {
         auto it = std::remove_if(employees.begin(), employees.end(), [&](const Employee &e) {
-            return e.get_firstName() == firstName && e.get_lastName() == lastName;
+            return e.getName() == firstName && e.getLastName() == lastName;
         });
         employees.erase(it, employees.end());
     }
 
     void sortByFirstName() {
         std::sort(employees.begin(), employees.end(), [](const Employee& e1, const Employee& e2) {
-            return e1.get_firstName() < e2.get_firstName();
+            return e1.getName() < e2.getName();
         });
     }
 
     void sortByLastName() {
         std::sort(employees.begin(), employees.end(), [](const Employee& e1, const Employee& e2) {
-            return e1.get_lastName() < e2.get_lastName();
+            return e1.getLastName() < e2.getLastName();
         });
     }
 
-    std::vector<Employee> get_employees() const {
+    std::vector<Employee> getAll() const {
         return employees;
     }
 };
