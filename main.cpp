add35e8cb526f22f11d920369596729ce9f2d503
LuchianovAntoniu-Aureliu <150062773+LuchianovAntoniu-Aureliu@users.noreply.github.com>
2024-05-28 19:36:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,28 @@
 #include <fstream>
 #include <string>
 #include "controller.cpp"
+#include "Employee.h"
+#include "User.h"
 using namespace std;
 
-void read(ifstream& inputFile, string& firstName, string& lastName, string& emailAddress, string& occupation, string& car, string& model) {
-    getline(inputFile, firstName, ' ');
-    getline(inputFile, lastName, ' ');
-    getline(inputFile, emailAddress, ' ');
-    getline(inputFile, occupation, ' ');
-    getline(inputFile, car, ' ');
-    getline(inputFile, model, ' ');
+void read(ifstream& inputFile, string name_, string lastName_, string position_, string abbreviation_, string phoneNumber_, string address_, string remarks_,
+          double salary_, string birthDate_) {
+    getline(inputFile, name_, ' ');
+    getline(inputFile, lastName_, ' ');
+    getline(inputFile, position_, ' ');
+    getline(inputFile, abbreviation_, ' ');
+    getline(inputFile, phoneNumber_, ' ');
+    getline(inputFile, address_, ' ');
+    getline(inputFile, remarks_, ' ');
+    inputFile >> salary_;
+    getline(inputFile, birthDate_, ' ');
 }
 
 int main() {
     EmployeeController controller;
     int nr;
-    string firstName, lastName, emailAddress, occupation, car, model;
+    double salary_;
+    string name_, lastName_, position_, abbreviation_, phoneNumber_, address_, remarks_, birthDate_;
     ifstream inputFile("file.txt");
     if(!inputFile.is_open()) {
         cerr << "Failed to open input file." << endl;
@@ -25,11 +32,12 @@ int main() {
     inputFile >> nr;
     inputFile.ignore();
     for(int i = 0; i < nr; ++i) {
-        read(inputFile, firstName, lastName, emailAddress, occupation, car, model);
-        Employee newEmployee(firstName, lastName, emailAddress, occupation, car, model);
+        read(inputFile, name_, lastName_, position_, abbreviation_, phoneNumber_, address_, remarks_, salary_, birthDate_);
+        Employee newEmployee(name_, lastName_, position_, abbreviation_, phoneNumber_, address_, remarks_, salary_, birthDate_);
         controller.addEmployee(newEmployee);
         inputFile.ignore();
-        cout << firstName << " " << lastName << " " << emailAddress << " " << occupation << " " << car << " " << model;
+        cout << name_ << " " << lastName_ << " " << position_ << " " << abbreviation_ << " " << phoneNumber_ << " " << address_ << " " <<
+        remarks_ << " " << salary_ << " " << birthDate_;
     }
     inputFile.close();
     return 0;
